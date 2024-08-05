815eb8c020385ad63de6c2a3b066878eede645f1
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-05 14:26:57 +0300

    Proje dosyası ekle.

---
+++ PatikaWeek4School/BaseKisi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4School
+{
+    public abstract class BaseKisi // her kişinin sahşp olduğu özelliği soyutluyorum
+    {
+        string _name;
+        string _surname;
+        public string name {get; set;    }
+        public string surname { get; set;}
+
+        public void Person(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            Console.WriteLine($"İsminiz {name} \n Soyisminiz {surname}");
+        }
+    
+    }
+    public class Student:BaseKisi
+    {
+        int _studentnum;
+        public int studentnum {get; set;}
+        public void Number(int studentnum)
+        {
+            _studentnum = studentnum;
+            Console.WriteLine($"Okul numaranız {studentnum}");
+        }
+        
+    }
+    public class Teacher:BaseKisi //Sınıfı miras almak 
+    {
+        double _wage;
+        public double wage {get; set;}
+        public void TeacherWage(double wage )
+        {
+            _wage = wage;
+            Console.WriteLine($"Maaşınız {wage}");
+        }
+    }
+}
---
+++ PatikaWeek4School/Program.cs
@@ -0,0 +1,9 @@
+﻿using PatikaWeek4School;
+
+Student student1 = new Student();
+student1.Person("Sezen", "Aksu");// miras alınan bilgiler
+student1.Number(55);
+
+Teacher teacher1 = new Teacher();
+teacher1.Person("Cem","Karaca");
+teacher1.TeacherWage(20000);
